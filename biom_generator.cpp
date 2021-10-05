85ec3b209b57b70c049ba70dd008eba3fcb85aa5
Vitalii Komisarenko <vitalii.komisarenko@yandex.ua>
2021-10-05 19:53:34 +0200

    Move biom_generator to header file since it is a template

--- biom_generator.cpp
+++ biom_generator.cpp
@@ -1,62 +1,10 @@
-#include <vector>
-#include <stdlib.h>
+#include "biom_generator.h"
 
 #define BIOM_DEMO // TODO: remove in production
 
 #ifdef BIOM_DEMO
 #include <iostream>
 #include <cstring>
-#endif
-
-/**
- * @brief basic biom generator
- *
- * This method randomly distributes biom centers over the map
- * Each tile obtains the type of the closest biom center
- */
-template <typename TILE_TYPE>
-std::vector<std::vector<TILE_TYPE>> generate_bioms(unsigned int seed, size_t width, size_t height, std::vector<TILE_TYPE> bioms)
-{
-    srand(seed);
-
-    struct biom_center_t {
-        int x;
-        int y;
-        TILE_TYPE tile;
-        int distance_multiplier;
-        biom_center_t(int _x, int _y, int _tile, int _distance_multiplier):
-            x(_x), y(_y), tile(_tile), distance_multiplier(_distance_multiplier) {};
-    };
-
-    std::vector<biom_center_t> biom_centers;
-    for (auto biom : bioms) {
-        biom_centers.push_back(biom_center_t(rand() % width, rand() % height, biom, rand() % 10 + 2));
-    }
-
-    std::vector<std::vector<TILE_TYPE>> map;
-
-    for (size_t x = 0; x < width; ++x) {
-        std::vector<TILE_TYPE> row;
-        for (size_t y = 0; y < height; ++y) {
-            TILE_TYPE closest_biom = bioms[0];
-            double min_distance = 90000000000000000000000000.0; // a big number
-            for (auto biom_center : biom_centers) {
-                auto x_diff = std::abs((double)(x - biom_center.x));
-                auto y_diff = std::abs((double)(y - biom_center.y));
-                double distance = (x_diff * x_diff + y_diff * y_diff) * biom_center.distance_multiplier;
-                if (distance < min_distance) {
-                    min_distance = distance;
-                    closest_biom = biom_center.tile;
-                }
-            }
-            row.push_back(closest_biom);
-        }
-        map.push_back(row);
-    }
-    return map;
-}
-
-#ifdef BIOM_DEMO
 
 static void write_vector(FILE *f, const std::vector<unsigned char> &vec) {
     fwrite(&vec[0], sizeof(unsigned char), vec.size(), f);
@@ -93,7 +41,7 @@ int main() {
         bioms.push_back(biom_string[i]);
     }
 
-    auto map = generate_bioms<char>(time(NULL), 1024, 1024, bioms);
+    auto map = Cinder::generate_bioms<char>(time(NULL), 1024, 1024, bioms);
     // command-line map output
     /*
     for (const auto &row : map) {
---
+++ biom_generator.h
@@ -0,0 +1,59 @@
+#ifndef _CINDER_BIOM_GENERATOR_H_
+#define _CINDER_BIOM_GENERATOR_H_
+
+#include <vector>
+#include <stdlib.h>
+
+namespace Cinder {
+
+/**
+ * @brief basic biom generator
+ *
+ * This method randomly distributes biom centers over the map
+ * Each tile obtains the type of the closest biom center
+ */
+template <typename TILE_TYPE>
+std::vector<std::vector<TILE_TYPE>> generate_bioms(unsigned int seed, size_t width, size_t height, std::vector<TILE_TYPE> bioms)
+{
+    srand(seed);
+
+    struct biom_center_t {
+        int x;
+        int y;
+        TILE_TYPE tile;
+        int distance_multiplier;
+        biom_center_t(int _x, int _y, TILE_TYPE _tile, int _distance_multiplier):
+            x(_x), y(_y), tile(_tile), distance_multiplier(_distance_multiplier) {};
+    };
+
+    std::vector<biom_center_t> biom_centers;
+    for (auto biom : bioms) {
+        biom_centers.push_back(biom_center_t(rand() % width, rand() % height, biom, rand() % 10 + 2));
+    }
+
+    std::vector<std::vector<TILE_TYPE>> map;
+
+    for (size_t x = 0; x < width; ++x) {
+        std::vector<TILE_TYPE> row;
+        for (size_t y = 0; y < height; ++y) {
+            TILE_TYPE closest_biom = bioms[0];
+            double min_distance = 90000000000000000000000000.0; // a big number
+            for (auto biom_center : biom_centers) {
+                auto x_diff = std::abs((double)(x - biom_center.x));
+                auto y_diff = std::abs((double)(y - biom_center.y));
+                double distance = (x_diff * x_diff + y_diff * y_diff) * biom_center.distance_multiplier;
+                if (distance < min_distance) {
+                    min_distance = distance;
+                    closest_biom = biom_center.tile;
+                }
+            }
+            row.push_back(closest_biom);
+        }
+        map.push_back(row);
+    }
+    return map;
+}
+
+}; // namespace Cinder
+
+#endif
