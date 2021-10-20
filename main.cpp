565f8ecfb42345a7f53e3da5d371e88b2c306fc1
Vitalii Komisarenko <vitalii.komisarenko@yandex.ua>
2021-10-20 12:06:10 +0200

    Add a helper class to parse command-line parameters

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <cstring>
 #include <iostream>
 
+#include "arg_parser.h"
 #include "map.h"
 #include "sin_parser.h"
 
@@ -12,27 +13,39 @@ constexpr uint32_t MAP_HEIGHT = MAP_WIDTH;
 void print_help() {
     std::cout << "Options: \n"
               << "    --run-sin-parser-tests\n"
-              << "    --generate-map <seed> (seed is a 64-bit number)\n";
+              << "    --debug-print-arguments (to test arg_parser class)\n"
+              << "    --generate-map\n"
+              << "        seed=<seed> (seed is a 64-bit number)\n";
 }
 
 int main(int argc, char **argv)
 {
-    if (argc == 1) {
+    arg_parser args(argc, argv);
+
+    if (args.cmd == "") {
         print_help();
         return 1;
     }
 
-    if (strcmp(argv[1], "--run-sin-parser-tests") == 0) {
+    if (args.cmd == "--debug-print-arguments") {
+        args.debug_print();
+        return 0;
+    }
+
+    if (args.cmd == "--run-sin-parser-tests") {
         Cinder::run_sin_parser_tests();
         return 0;
     }
 
-    if (strcmp(argv[1], "--generate-map") == 0) {
-        if (argc != 3) {
-            print_help();
-            return 1;
+    if (args.cmd == "--generate-map") {
+        uint64_t seed;
+        if (args.params.find("seed") == args.params.end()) {
+            srand(time(NULL));
+            seed = rand();
         }
-        uint64_t seed = std::strtoul(argv[2], NULL, 10);
+        else {
+            seed = std::strtoul(args.params["seed"].c_str(), NULL, 10);
+        }
         std::cout << "Generating map with seed " << seed << "\n";
         Cinder::Map map(MAP_WIDTH, MAP_HEIGHT, seed);
 
