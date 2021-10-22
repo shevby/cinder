02d74a81b7cc42fa953ae851dd32a0d67df3e93b
Vitalii Komisarenko <vitalii.komisarenko@yandex.ua>
2021-10-22 14:30:20 +0200

    Map generation: PoC river generation (with tile replacement)

--- map.cpp
+++ map.cpp
@@ -1,8 +1,10 @@
 #include <cstdio>
 #include <math.h>
+#include <vector>
 
 #include "map.h"
 #include "perlin_noise.h"
+#include "stable_random.h"
 
 namespace Cinder {
 
@@ -10,13 +12,24 @@ static float interpolate(float left, float right, float quotient) {
     return (1 - quotient) * left + quotient * right;
 }
 
+float Map::get_altitude(uint32_t x, uint32_t y) {
+    return 0.25 * (
+        perlin(seed + 100000 + 0.1 * x + 0.111, seed + 10000 + 0.1 * y + 0.1111) +
+        perlin(seed + 200000 + 0.01 * x + 0.111, seed + 20000 + 0.01 * y + 0.1111) +
+        perlin(seed + 300000 + 0.005 * x + 0.111, seed + 30000 + 0.005 * y + 0.1111) +
+        perlin(seed + 400000 + 0.001 * x + 0.111, seed + 40000 + 0.001 * y + 0.1111)
+    );
+}
+
 Map::Map(const MapConfig &cfg) :
     width(cfg.width),
     height(cfg.height),
     map(new uint8_t[width * height]),
-    mapType(MapTypes::WORLD_MAP)
+    rivers(new River[width * height]),
+    mapType(MapTypes::WORLD_MAP),
+    seed(cfg.seed)
 {
-    auto seed = cfg.seed;
+    Random rand(seed);
 
     #pragma omp parallel for
     for (size_t x = 0; x < width; ++x) {
@@ -29,12 +42,7 @@ Map::Map(const MapConfig &cfg) :
                                     0.5 - latitude,
                                     cfg.wet_equator
                                 );
-            float altitude    = 0.25 * (
-                perlin(seed + 100000 + 0.1 * x + 0.111, seed + 10000 + 0.1 * y + 0.1111) +
-                perlin(seed + 200000 + 0.01 * x + 0.111, seed + 20000 + 0.01 * y + 0.1111) +
-                perlin(seed + 300000 + 0.005 * x + 0.111, seed + 30000 + 0.005 * y + 0.1111) +
-                perlin(seed + 400000 + 0.001 * x + 0.111, seed + 40000 + 0.001 * y + 0.1111)
-            );
+            float altitude    = get_altitude(x, y);
 
             Biomes biom = altitude < cfg.sea_level ? temperature > -0.3 ? Biomes::WATER : Biomes::GLACIER
                         : altitude > cfg.high_rock_level ? Biomes::HIGH_ROCK
@@ -46,6 +54,102 @@ Map::Map(const MapConfig &cfg) :
                         : Biomes::DESERT;
 
             map[x * height + y] = static_cast<uint8_t>(biom);
+        }
+    }
+
+    for (size_t x = 0; x < width; ++x) {
+        for (size_t y = 0; y < height; ++y) {
+            // skip tile if there is a river
+            if (rivers[x * height + y].riverExit != TileBorder::NONE)
+                continue;
+
+            // skip water tile
+            if (map[x * height + y] == static_cast<uint8_t>(Biomes::WATER))
+                continue;
+            if (map[x * height + y] == static_cast<uint8_t>(Biomes::GLACIER))
+                continue;
+
+            // find a tile on a shore
+
+            std::vector<std::pair<uint32_t, uint32_t>> water_nearby;
+            TileBorder border;
+            if (x >= 1 && map[(x-1) * height + y] == static_cast<uint8_t>(Biomes::WATER)) {
+                water_nearby.push_back(std::make_pair(x-1, y));
+                border = TileBorder::LEFT;
+            }
+            if (x < height - 1 && map[(x+1) * height + y] == static_cast<uint8_t>(Biomes::WATER)) {
+                water_nearby.push_back(std::make_pair(x+1, y));
+                border = TileBorder::RIGHT;
+            }
+            if (y >= 1 && map[x * height + y - 1] == static_cast<uint8_t>(Biomes::WATER)) {
+                water_nearby.push_back(std::make_pair(x, y-1));
+                border = TileBorder::TOP;
+            }
+            if (y < width - 1 && map[x * height + y + 1] == static_cast<uint8_t>(Biomes::WATER)) {
+                water_nearby.push_back(std::make_pair(x, y+1));
+                border = TileBorder::BOTTOM;
+            }
+
+            if (water_nearby.size() == 0)
+                continue;
+
+            if (rand.rand() < (uint64_t(-1)) * cfg.river_density)
+                continue;
+
+            // choose a random water neighbour as river mouth
+            size_t idx = rand.rand() % water_nearby.size();
+            rivers[x * height + y].riverExit = border;
+
+            uint32_t x_prev = x;
+            uint32_t y_prev = y;
+
+            while (true) {
+                // choose the next tile
+                std::vector<std::pair<uint32_t, uint32_t>> neightbour_candidates = {
+                    {x_prev - 1, y_prev},
+                    {x_prev + 1, y_prev},
+                    {x_prev, y_prev - 1},
+                    {x_prev, y_prev + 1}
+                };
+
+                std::vector<std::pair<uint32_t, uint32_t>> neightbours_to_choose;
+                for (const auto &candidate : neightbour_candidates) {
+                    uint32_t candidate_x = candidate.first;
+                    uint32_t candidate_y = candidate.second;
+
+                    if ((candidate_x < 0) || (candidate_x >= height) || (candidate_y < 0) || (candidate_y >= width)) {
+                        continue;
+                    }
+
+                    if (get_altitude(candidate_x, candidate_y) <= get_altitude(x_prev, y_prev)) {
+                        continue;
+                    }
+
+                    neightbours_to_choose.push_back(candidate);
+                }
+
+                if (neightbours_to_choose.size() == 0) {
+                    // Impossible to add yet another tile to the river
+                    break;
+                }
+
+                auto next_tile = neightbours_to_choose[rand.rand() % neightbours_to_choose.size()];
+                // TODO fix river direction
+                rivers[x_prev * height + y_prev].riverEntry = TileBorder::TOP;
+                rivers[next_tile.first * height + next_tile.second].riverExit = TileBorder::BOTTOM;
+
+                x_prev = next_tile.first;
+                y_prev = next_tile.second;
+            }
+        }
+    }
+
+    for (size_t x = 0; x < width; ++x) {
+        for (size_t y = 0; y < height; ++y) {
+            auto idx = x * height + y;
+            if (rivers[idx].riverExit != TileBorder::NONE) {
+                map[idx] = static_cast<uint8_t>(Biomes::WATER);
+            }
         }
     }
 }
