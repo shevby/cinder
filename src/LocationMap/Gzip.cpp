9b4f759d016f0a189d911537e82beead5b47cc66
Vitalii Komisarenko <vitalii.komisarenko@gmail.com>
2022-12-27 12:59:37 +0100

    Web Server: Add skeleton for Gzip compression

--- src/LocationMap/Gzip.cpp
+++ src/LocationMap/Gzip.cpp
@@ -1,5 +1,13 @@
 #include "Gzip.h"
 
+static std::string unit32_to_string(uint32_t num) {
+    return std::string()
+         + static_cast<char>(num >> 24)
+         + static_cast<char>((num >> 16) & 0xFF)
+         + static_cast<char>((num >> 8) & 0xFF)
+         + static_cast<char>(num & 0xFF);
+}
+
 std::string adler_32_checksum(const std::string &content) {
     constexpr uint32_t MOD_ADLER_32 = 65521;
 
@@ -11,25 +19,28 @@ std::string adler_32_checksum(const std::string &content) {
         B += A % MOD_ADLER_32;
     }
 
-    uint32_t res = (B << 16) | A;
-
-    return std::string()
-         + static_cast<char>(res >> 24)
-         + static_cast<char>((res >> 16) & 0xFF)
-         + static_cast<char>((res >> 8) & 0xFF)
-         + static_cast<char>(res & 0xFF);
+    return unit32_to_string((B << 16) | A);
 }
 
-std::string crc32(const std::string &content) {
+std::string deflate_payload(const std::string &content) {
 }
 
 std::string deflate(const std::string &content) {
 }
 
+std::string crc32(const std::string &content) {
+}
+
 std::string gzip_compress(const std::string &content) {
     const std::string MAGIC = "\x1F\x8B";
-    const std::string COMPRESSION_METHOD = "\x08";
+    const std::string COMPRESSION_METHOD = "\x08"; // 0x08 = DEFLATE
+    std::string flags = "\0";
     const std::string TIMESTAMP = "\x00\x00\x00\x00";
-    std::string res = MAGIC + COMPRESSION_METHOD;
+    std::string compression_flags = "\0";
+    const std::string OS_ID = "\xFF"; // 0xFF = unknown
+
+    std::string header = MAGIC + COMPRESSION_METHOD + flags + TIMESTAMP + compression_flags + OS_ID;
+
+    std::string res = header + deflate_payload(content) + crc32(content) + unit32_to_string(content.size());
     return res;
 }
--- src/LocationMap/Gzip.h
+++ src/LocationMap/Gzip.h
@@ -10,6 +10,21 @@
 std::string adler_32_checksum(const std::string &content);
 
 /**
+ * @brief Compress with DEFLATE algorithm and return only payload part
+ */
+std::string deflate_payload(const std::string &content);
+
+/**
+ * @brief Compress with DEFLATE algorithm
+ */
+std::string deflate(const std::string &content);
+
+/**
+ * @brief calculate CRC-32 sum used by Gzip
+ */
+std::string crc32(const std::string &content);
+
+/**
  * @brief Compress payload in gzip-compartible format
  */
 std::string gzip_compress(const std::string &content);
