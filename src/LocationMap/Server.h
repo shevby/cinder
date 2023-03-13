fdc4d9c9cac3e6d07434e150c6f89fe3a6831edd
Vitalii Komisarenko <vitalii.komisarenko@mind.be>
2023-03-13 14:10:20 +0200

    Location Map: Add code for server that keeps connection

---
+++ src/LocationMap/Server.h
@@ -0,0 +1,27 @@
+#ifndef LOCATION_MAP_SERVER_H
+#define LOCATION_MAP_SERVER_H
+
+#include <string>
+
+class Server {
+public:
+    Server(int port);
+    ~Server();
+    void update_data(const std::string& data);
+
+private:
+    void accept_connections();
+    void send_data(int client_fd, const std::string& data);
+
+    int port_;
+    int server_fd_ = -1;
+    bool running_;
+    std::string data_;
+    std::vector<int> client_fds_;
+    std::thread accept_thread_;
+    std::mutex client_fds_mutex_;
+    std::mutex data_mutex_;
+};
+
+#endif // LOCATION_MAP_SERVER_H
+
